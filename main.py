05ff72f0831e90107531f7cc26f758cac1718c01
dangjin <dangjin798@yeah.net>
2024-01-09 11:29:48 +0800

    Update main.py
    增加cb

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ app = Flask(__name__)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route('/cb', methods=['POST'])
+def handle_post_request():
+    # 这里可以添加你的业务逻辑代码
+    data = request.get_json()  # 如果需要解析JSON数据的话
+    # 处理data...
+    print(data)
+
+    response = {"status": "success", "message": "Post request received successfully"}
+    
+    return jsonify(response), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
